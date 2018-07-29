60c72a224b7a5f9bf1d0aab7f50688026839e13f
“Michael <michael_speer@comcast.com>
2018-07-29 17:25:19 -0500

    init

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var eightBallImage: UIImageView!
+    var eightBallRandom : Int = 1
+    
+    let eightBallArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        changeEightBallImage()
     }
-
+    
+    func generateRandom(){
+        eightBallRandom = Int(arc4random_uniform(5))
+    }
+    
+    func changeEightBallImage(){
+        generateRandom()
+        
+        eightBallImage.image = UIImage(named: eightBallArray[eightBallRandom])
+    }
+    
+    @IBAction func shakeButton(_ sender: UIButton) {
+        changeEightBallImage()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        changeEightBallImage()
+    }
+    
 
 
 }
